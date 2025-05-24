37882d4a0335eedf006a6b7118e54d64be29216f
Sourav6803 <souravbhukta8@gmail.com>
2025-05-24 07:45:01 +0530

    clerk removed hed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import Link from 'next/link';
-import { UserButton, SignInButton, useUser } from '@clerk/nextjs';
 import { Menu } from 'lucide-react';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
-  const { isSignedIn, user } = useUser();
+  const { isAuthenticated, user } = useSelector((state: any) => state.user);
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
@@ -19,14 +19,16 @@ const Header = () => {
 
       {/* Desktop Auth Section */}
       <div className="hidden md:flex items-center space-x-4">
-        {isSignedIn ? (
-          <UserButton afterSignOutUrl="/" />
+        {isAuthenticated ? (
+          // <UserButton afterSignOutUrl="/" />
+
+          <div></div>
         ) : (
-          <SignInButton mode="modal">
+          <div >
             <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition">
               Sign In
             </button>
-          </SignInButton>
+          </div>
         )}
       </div>
 
@@ -40,14 +42,14 @@ const Header = () => {
       {/* Mobile Dropdown */}
       {menuOpen && (
         <div className="absolute top-16 right-4 bg-white border shadow-lg rounded-md p-4 w-40 z-50 md:hidden">
-          {isSignedIn ? (
-            <UserButton afterSignOutUrl="/" />
+          {isAuthenticated? (
+            <button>Login </button>
           ) : (
-            <SignInButton mode="modal">
+           
               <button className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition">
                 Sign In
               </button>
-            </SignInButton>
+            
           )}
         </div>
       )}
